be3e54cd361274ed9b275ea5c653609c654264ef
fe-admin <13488881765@163.com>
2021-06-01 16:15:41 +0800

    动态刷新menu不更新问

--- src/Aside/MenuRender.jsx
+++ src/Aside/MenuRender.jsx
@@ -1,16 +1,8 @@
 const MenuRender = {
-  name: "MenuRender",
+  name: 'MenuRender',
   functional: true,
   render(h, context) {
-    const {
-      defaultActive,
-      defaultOpeneds,
-      uniqueOpened,
-      collapse,
-      collapseTransition,
-      menus,
-      redirect,
-    } = context.props;
+    const { defaultActive, defaultOpeneds, uniqueOpened, collapse, collapseTransition, menus, redirect } = context.props
     return (
       <el-menu
         default-active={defaultActive}
@@ -21,17 +13,17 @@ const MenuRender = {
       >
         {headerRender(h, { menus, redirect })}
       </el-menu>
-    );
+    )
   },
-};
+}
 
 const headerRender = (h, { menus, redirect }) => {
-  const domArr = [];
-  if (!Array.isArray(menus)) return domArr;
+  const domArr = []
+  if (!Array.isArray(menus)) return domArr
   menus.forEach((item) => {
     if (item.children && item.children.length) {
       domArr.push(
-        <el-submenu index={item.id}>
+        <el-submenu index={item.id + ''} key={item.name + item.id}>
           <template slot="title">
             {item.meta.icon ? <svg-icon iconName={item.meta.icon} /> : null}
             {item.meta.title}
@@ -41,24 +33,20 @@ const headerRender = (h, { menus, redirect }) => {
           </template>
           {headerRender(h, { menus: item.children, redirect })}
         </el-submenu>
-      );
+      )
     } else {
       domArr.push(
-        <el-menu-item
-          index={item.id}
-          class="item-children"
-          vOn:click={() => redirect(item)}
-        >
+        <el-menu-item index={item.id} key={item.name + item.id} class="item-children" vOn:click={() => redirect(item)}>
           {item.meta.icon ? <svg-icon iconName={item.meta.icon} /> : null}
           <span slot="title" class="hide">
             <span class="txt">{item.meta.title}</span>
           </span>
           {item.meta.title}
         </el-menu-item>
-      );
+      )
     }
-  });
-  return domArr;
-};
+  })
+  return domArr
+}
 
-export default MenuRender;
+export default MenuRender
