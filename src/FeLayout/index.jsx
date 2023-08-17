b7e735c48ba5ea9515c7278d8e77ae6f67ae3277
fe-admin <13488881765@163.com>
2023-08-17 08:31:59 +0800

    优化样式

--- src/FeLayout/index.jsx
+++ src/FeLayout/index.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: long
  * @Date: 2021-11-04 09:24:08
- * @LastEditTime: 2022-08-30 14:05:35
+ * @LastEditTime: 2023-06-20 10:30:50
  * @LastEditors: long
  * @Description:
  * @FilePath: /website/Users/long/Documents/workspace/fe-admin-component/src/FeLayout/index.jsx
@@ -11,7 +11,6 @@ import Header from '../Header'
 import Footer from '../Footers'
 import Aside from '../Aside'
 import Collapse from '../Collapse'
-import TagView from '../TagViews'
 import { getComponentFromProp, getAlive } from '../utils'
 import './style.scss'
 
@@ -19,15 +18,14 @@ const FeLayout = {
   name: 'FeLayout',
   functional: true,
   render(h, context) {
-    console.info(context)
     const asideTitleRender = getComponentFromProp(context, 'asideTitle')
     const asideExtraRender = getComponentFromProp(context, 'asideExtra')
     const extraRender = getComponentFromProp(context, 'extra')
     const footerRender = getComponentFromProp(context, 'footer')
-    const { isCollapse, toggleCollapse, showTagViews } = context.props
+    const { isCollapse, toggleCollapse } = context.props
     return (
-      <el-container class={isCollapse ? 'fe-layout-collapse' : 'fe-layout'}>
-        <el-header class="fe-header" height="48px">
+      <el-container class="fe-container">
+        <el-header class="fe-header">
           <Header
             {...{
               props: { ...context.props },
@@ -35,8 +33,8 @@ const FeLayout = {
             }}
           ></Header>
         </el-header>
-        <el-container class="fe-container">
-          <el-aside class="el-aside-menu" width="180px">
+        <el-container class={isCollapse ? 'fe-layout-collapse' : 'fe-layout'}>
+          <el-aside class="el-aside-menu" width="220px">
             <Aside
               {...{
                 props: { ...context.props },
@@ -50,8 +48,6 @@ const FeLayout = {
           </el-aside>
           <el-container class="main-content">
             <el-main>
-              {showTagViews ? <TagView {...{ props: { ...context.props } }}></TagView> : null}
-
               <keep-alive {...{ props: { max: 5, include: getAlive(context.props) } }}>
                 <router-view />
               </keep-alive>
@@ -60,8 +56,8 @@ const FeLayout = {
               <Footer>{footerRender}</Footer>
             </el-footer>
           </el-container>
+          <div>{extraRender}</div>
         </el-container>
-        <div>{extraRender}</div>
       </el-container>
     )
   },
--- src/SvgIcon/index.jsx
+++ src/SvgIcon/index.jsx
@@ -1,27 +1,36 @@
+/*
+ * @Author: long
+ * @Date: 2021-11-04 09:24:08
+ * @LastEditTime: 2022-12-21 15:05:14
+ * @LastEditors: long
+ * @Description:
+ * @FilePath: /website/Users/long/Documents/workspace/fe-admin-component/src/SvgIcon/index.jsx
+ *
+ */
 const SvgIcon = {
-  name: "SvgIcon",
-  props: ["className", "iconName"],
+  name: 'SvgIcon',
+  props: ['className', 'iconName'],
   data() {
-    return {};
+    return {}
   },
   render(h) {
-    const { className, iconName } = this;
-    const icoName = `#${iconName}`;
+    const { className, iconName } = this
+    const icoName = `#${iconName}`
     return (
-      <svg aria-hidden="true" class={className ? className : "icon"}>
+      <svg aria-hidden="true" class={className ? className : 'icon'}>
         {this.setUse(h, icoName)}
       </svg>
-    );
+    )
   },
   methods: {
     setUse(h, name) {
-      return h("use", {
-        attrs: { "xlink:href": name },
-      });
+      return h('use', {
+        attrs: { 'xlink:href': name },
+      })
     },
   },
-};
-SvgIcon.install = function(Vue) {
-  Vue.component(SvgIcon.name, SvgIcon);
-};
-export default SvgIcon;
+}
+SvgIcon.install = function (Vue) {
+  Vue.component(SvgIcon.name, SvgIcon)
+}
+export default SvgIcon
